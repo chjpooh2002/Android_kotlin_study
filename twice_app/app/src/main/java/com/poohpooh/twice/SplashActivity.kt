dc2a388f6ff907c3be2bf7944bda40d4ae8bee6f
HongJune <chjpooh2002@naver.com>
2023-04-11 02:09:51 +0900

    android 폴더 이름 변경 및 새로운 twice폴더 추가

---
+++ twice_app/app/src/main/java/com/poohpooh/twice/SplashActivity.kt
@@ -0,0 +1,19 @@
+package com.poohpooh.twice
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+@Suppress("DEPRECATION")
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            startActivity(Intent(this,MainActivity::class.java))
+            finish()
+        },3000)
+    }
+}
